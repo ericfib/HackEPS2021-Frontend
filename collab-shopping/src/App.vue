cc0ed81a9bdfb6bf4b8f074918b799a8ceaea459
EncryptEx <limusina10@gmail.com>
2021-11-27 17:41:42 +0100

    Revert "added list items shop"
    This reverts commit 131eb1138e18362be6d4874bf3c2294ca9f18aeb.

--- collab-shopping/src/App.vue
+++ collab-shopping/src/App.vue
@@ -1,15 +1,12 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import listitems from './components/listitems.vue'
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <listitems name="Hello Vue 3 + Vite" />
-  <listitems description="Hello Vue 3 + Vite" />
-  <listitems msg="Hello Vue 3 + Vite" />
-  <listitems imageurls="asdasds" />
+  <HelloWorld msg="Hello Vue 3 + Vite" />
 </template>
 
 <style>
