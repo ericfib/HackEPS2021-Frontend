1a96376b488e113246a680d03c3a869fe63edab0
EncryptEx <limusina10@gmail.com>
2021-11-28 07:42:05 +0100

    Cart made

--- collab-shopping/src/components/listitems.vue
+++ collab-shopping/src/components/listitems.vue
@@ -19,7 +19,53 @@ const count = ref(0)
       </div>
     </div>
     </header>
-<!-- header opcional?--> 
+  <!-- header opcional?--> 
+  <section class="text-gray-600 body-font">
+  <div class="container px-5 py-24 mx-auto">
+    <div class="flex flex-col text-center w-full mb-20">
+      <h1 class="sm:text-4xl text-3xl font-medium title-font mb-2 text-gray-900">Cart</h1>
+      <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Here's what you have added to your beautiful shopping cart. </p>
+    </div>
+    <div class="lg:w-2/3 w-full mx-auto overflow-auto">
+      <table class="table-auto w-full text-left whitespace-no-wrap">
+        <thead>
+          <tr>
+            <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">Item</th>
+            <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">Description</th>
+            <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">Quantity</th>
+            <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">Price</th>
+          </tr>
+        </thead>
+        <tbody>
+
+
+
+          <tr v-for="item in info" :key="item">
+            <td class="px-4 py-3">{{name}}</td>
+            <td class="px-4 py-3">{{description}}</td>
+            <td class="px-4 py-3">{{quantity}}</td>
+            <td class="px-4 py-3 text-lg text-gray-900">{{price}} </td>
+          </tr>
+
+
+
+        </tbody>
+      </table>
+    </div>
+    <div class="flex pl-4 mt-4 lg:w-2/3 w-full mx-auto">
+      <a class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More
+        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
+          <path d="M5 12h14M12 5l7 7-7 7"></path>
+        </svg>
+      </a>
+      <button class="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Button</button>
+    </div>
+  </div>
+</section>
+
+
+
+
   <button @click="goTo('/')">Go to home</button>
 
 
